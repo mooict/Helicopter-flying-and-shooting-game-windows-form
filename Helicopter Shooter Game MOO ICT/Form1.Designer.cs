4b7e2d43c04cfd0d690bd6a604808921cfa7e55d
Anhar Ali <67914277+mooict@users.noreply.github.com>
2021-01-03 17:56:45 +0000

    Add files via upload

---
+++ Helicopter Shooter Game MOO ICT/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Helicopter_Shooter_Game_MOO_ICT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pillar1 = new System.Windows.Forms.PictureBox();
+            this.pillar2 = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.ufo = new System.Windows.Forms.PictureBox();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.txtScore = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pillar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pillar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ufo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pillar1
+            // 
+            this.pillar1.Image = global::Helicopter_Shooter_Game_MOO_ICT.Properties.Resources.pillar;
+            this.pillar1.Location = new System.Drawing.Point(579, -7);
+            this.pillar1.Name = "pillar1";
+            this.pillar1.Size = new System.Drawing.Size(61, 201);
+            this.pillar1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pillar1.TabIndex = 0;
+            this.pillar1.TabStop = false;
+            this.pillar1.Tag = "pillar";
+            // 
+            // pillar2
+            // 
+            this.pillar2.Image = global::Helicopter_Shooter_Game_MOO_ICT.Properties.Resources.pillar;
+            this.pillar2.Location = new System.Drawing.Point(253, 292);
+            this.pillar2.Name = "pillar2";
+            this.pillar2.Size = new System.Drawing.Size(61, 201);
+            this.pillar2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pillar2.TabIndex = 0;
+            this.pillar2.TabStop = false;
+            this.pillar2.Tag = "pillar";
+            // 
+            // player
+            // 
+            this.player.Image = global::Helicopter_Shooter_Game_MOO_ICT.Properties.Resources.Halicopter;
+            this.player.Location = new System.Drawing.Point(102, 119);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(100, 54);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.player.TabIndex = 1;
+            this.player.TabStop = false;
+            // 
+            // ufo
+            // 
+            this.ufo.Image = global::Helicopter_Shooter_Game_MOO_ICT.Properties.Resources.alien1;
+            this.ufo.Location = new System.Drawing.Point(818, 259);
+            this.ufo.Name = "ufo";
+            this.ufo.Size = new System.Drawing.Size(68, 72);
+            this.ufo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.ufo.TabIndex = 2;
+            this.ufo.TabStop = false;
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.MainTimerEvent);
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.ForeColor = System.Drawing.Color.White;
+            this.txtScore.Location = new System.Drawing.Point(13, 13);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(88, 24);
+            this.txtScore.TabIndex = 3;
+            this.txtScore.Text = "Score: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(925, 478);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.ufo);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.pillar2);
+            this.Controls.Add(this.pillar1);
+            this.Name = "Form1";
+            this.Text = "Helicopter Shooter Game MOO ICT";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pillar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pillar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ufo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pillar1;
+        private System.Windows.Forms.PictureBox pillar2;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox ufo;
+        private System.Windows.Forms.Timer GameTimer;
+        private System.Windows.Forms.Label txtScore;
+    }
+}
+
---
+++ Helicopter Shooter Game MOO ICT/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Helicopter_Shooter_Game_MOO_ICT
+{
+    public partial class Form1 : Form
+    {
+
+        bool goUp, goDown, shot, gameOver;
+
+        int score = 0;
+        int speed = 8;
+        int UFOspeed = 10;
+
+        Random rand = new Random();
+
+        int playerSpeed = 7;
+        int index = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MainTimerEvent(object sender, EventArgs e)
+        {
+            txtScore.Text = "Score: " + score;
+
+            if (goUp == true && player.Top > 0)
+            {
+                player.Top -= playerSpeed;
+            }
+            if (goDown == true && player.Top + player.Height < this.ClientSize.Height)
+            {
+                player.Top += playerSpeed;
+            }
+
+            ufo.Left -= UFOspeed;
+
+            if (ufo.Left + ufo.Width < 0)
+            {
+                ChangeUFO();
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "pillar")
+                {
+                    x.Left -= speed;
+
+
+                    if (x.Left < -200)
+                    {
+                        x.Left = 1000;
+                    }
+
+                    if (player.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        GameOver();
+                    }
+
+                }
+
+                if (x is PictureBox && (string)x.Tag == "bullet")
+                {
+                    x.Left += 25;
+
+                    if (x.Left > 900)
+                    {
+                        RemoveBullet(((PictureBox)x));
+                    }
+
+
+                    if (ufo.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        RemoveBullet(((PictureBox)x));
+                        score += 1;
+                        ChangeUFO();
+                    }
+
+                }
+
+            }
+
+
+            if (player.Bounds.IntersectsWith(ufo.Bounds))
+            {
+                GameOver();
+            }
+
+            if (score > 10)
+            {
+                speed = 12;
+                UFOspeed = 18;
+            }
+
+
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = true;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = true;
+            }
+
+            if (e.KeyCode == Keys.Space && shot == false)
+            {
+                MakeBullet();
+                shot = true;
+            }
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = false;
+            }
+            if (shot == true)
+            {
+                shot = false;
+            }
+
+            if (e.KeyCode == Keys.Enter && gameOver == true)
+            {
+                RestartGame();
+            }
+
+        }
+
+        private void RestartGame()
+        {
+
+            goUp = false;
+            goDown = false;
+            shot = false;
+            gameOver = false;
+            score = 0;
+            speed = 8;
+            UFOspeed = 10;
+
+            txtScore.Text = "Score: " + score;
+
+            ChangeUFO();
+
+            player.Top = 119;
+
+            pillar1.Left = 579;
+            pillar2.Left = 253;
+
+            GameTimer.Start();
+
+
+        }
+
+        private void GameOver()
+        {
+            GameTimer.Stop();
+            txtScore.Text = "Score: " + score + " Game over, press enter to retry!";
+            gameOver = true;
+        }
+
+        private void RemoveBullet(PictureBox bullet)
+        {
+            this.Controls.Remove(bullet);
+            bullet.Dispose();
+        }
+
+        private  void MakeBullet()
+        {
+            PictureBox bullet = new PictureBox();
+            bullet.BackColor = Color.Maroon;
+            bullet.Height = 5;
+            bullet.Width = 10;
+
+            bullet.Left = player.Left + player.Width;
+            bullet.Top = player.Top + player.Height / 2;
+
+            bullet.Tag = "bullet";
+
+            this.Controls.Add(bullet);
+
+        }
+
+        private void ChangeUFO()
+        {
+
+
+            if (index > 3)
+            {
+                index = 1;
+            }
+            else
+            {
+                index += 1;
+            }
+
+
+            switch (index)
+            {
+                case 1:
+                    ufo.Image = Properties.Resources.alien1;
+                    break;
+                case 2:
+                    ufo.Image = Properties.Resources.alien2;
+                    break;
+                case 3:
+                    ufo.Image = Properties.Resources.alien3;
+                    break;
+            }
+
+            ufo.Left = 1000;
+
+            ufo.Top = rand.Next(20, this.ClientSize.Height - ufo.Height);
+
+        }
+    }
+}
